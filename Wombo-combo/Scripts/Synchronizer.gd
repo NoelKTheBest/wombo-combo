9a6a95f9923e9458fee4a57791396d11b458d3c1
NKDevArt <72371650+NoelKTheBest@users.noreply.github.com>
2023-10-18 19:03:34 -0700

    Find data by seeking to index

--- Wombo-combo/Scripts/Synchronizer.gd
+++ Wombo-combo/Scripts/Synchronizer.gd
@@ -1,56 +1,11 @@
 extends Node
 
-# 0 - start 1
-# 1 - start stab
-# 2 - start stab down
-# 3 - start stab up
-# 4 - start sword
-# 5 - start sword 1
-# 6 - start sword 2
-# 7 - start sword down
-# 8 - start sword up
-var starter_positions = [Vector2(12, 0), Vector2(10, 0), Vector2(0, 10), Vector2(0, -8),
- Vector2(10, 0), Vector2(10, 0), Vector2(10, 0), Vector2(0, 0), Vector2(1, 0)]
-
-# 0 - start 1
-# -
-# -
-# -
-# 1 - start sword
-# -
-# 2 - start sword 2
-# 3 - start sword down
-# 4 - start sword up
-var starter_knockback = [Vector2(0, -10), Vector2(10, 0), Vector2(10, 0), Vector2(0, 10), Vector2(0, -10)]
-
 var starters = []
 
 var file = 'res://starters.txt'
 
-# get_position() returns the number of bytes away from the start of the file the cursor is, not the number of bits
-var seeker
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var f = FileAccess.open(file, FileAccess.READ)
-	# i may want to get all starter animations first
-	var content = f.get_line()
-	seeker = f.get_position()
-	
-	# Get number of animations
-	for i in int(content):
-		print(i)
-		starters.append(Anim.new())
-	print(starters)
-	
-	content = f.get_8()
-	seeker = f.get_position()
-	print("seeker: " + str(seeker))
-	print(char(content))
-	while !f.eof_reached():
-		#f.seek(f.get_position() + 1)
-		print(char(f.get_8()))
-	
 	# Get all starter animations
 	var animations = get_parent().sprite_frames.get_animation_names()
 	var starter_animations = []
@@ -58,11 +13,13 @@ func _ready():
 		if anim.contains("Start"):
 			starter_animations.append(anim)
 	print(starter_animations)
-	print(starter_animations.size())
 	
-	# file.seek(file.get_position() + n) skips n number of bytes
-	# file.get_8() automatically advances cursor forward 1 byte
-	# file.get_line() retrieves the entire line and advances cursor to the beginning of the next line
+	for i in starter_animations.size():
+		starters.append(Anim.new())
+	
+	print(seek_to_index(0))
+	print(seek_to_index(5))
+	print(seek_to_index(9))
 	
 
 
@@ -71,10 +28,65 @@ func _process(delta):
 	pass
 
 
-func seek_to_index(index):
+func find_property(property: String):
+	# Look for correct property out of the 3 possible choices
+	
+	# Pass file and cursor info to seek_to_index()
+	
+	print(property)
+
+
+func seek_to_index(index: int):
 	# function responsible for moving the file cursor to the correct point to grab the data for the 
 	#	current animation
-	pass
+	var f = FileAccess.open(file, FileAccess.READ)
+	f.seek(2)
+	
+	var x = ""
+	var y = ""
+	
+	f.seek(f.get_position() + 2)
+	
+	# Seek to index
+	var content = f.get_8() # seek 5
+	while int(char(content)) != index:
+		content = char(f.get_8())
+		while content != ';':
+			content = char(f.get_8())
+		
+		content = f.get_8()
+		
+		# Return zero vector if index is not found
+		if char(content) == ']':
+			f.close()
+			return Vector2(0, 0)
+	
+	# Get Data
+	if int(char(content)) == index:
+		f.seek(f.get_position() + 1)
+
+		
+		# Get x value
+		content = char(f.get_8())
+		while content != ',':
+			x = x + content
+			content = char(f.get_8())
+		
+		x = int(x)
+		
+		f.seek(f.get_position() + 1)
+		
+		# Get y value
+		content = char(f.get_8())
+		while content != ')':
+			y = y + content
+			content = char(f.get_8())
+		
+		y = int(y)
+	
+	f.close()
+	
+	return Vector2(x, y) if typeof(x) == typeof(0) else Vector2(0, 0)
 
 
 class Anim:
