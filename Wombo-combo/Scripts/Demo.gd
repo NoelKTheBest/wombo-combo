e8d50bc2e6137986d185fb767998dc20bd655308
NoelKTheBest <72371650+NoelKTheBest@users.noreply.github.com>
2025-02-27 21:20:20 -0800

    Last commit

---
+++ Wombo-combo/Scripts/Demo.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# starter combinations
+var starters = ['Slash 1']
+var attacks = ['Slash 2']
+var finishers = ['Slash 3']
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
